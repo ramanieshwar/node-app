ea0c6ab67839a9f8984b91f3f3f4de6d1f7b2f2b
Hema Durga Ramani <62502840+ramanieshwar@users.noreply.github.com>
2025-06-01 09:57:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from Jenkins on AWS!'));
+app.get('/', (req, res) => res.send('Hello eshwar how are you!'));
 app.listen(3000, () => console.log('App running on port 3000'));
 
